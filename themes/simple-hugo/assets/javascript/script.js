44dd2d32059fdb1aae49a194efe00143feaeed79
robson.klein <robson.klein@digitaldesk.com.br>
2020-03-27 17:14:04 -0300

    ajustes layout

--- themes/simple-hugo/assets/javascript/script.js
+++ themes/simple-hugo/assets/javascript/script.js
@@ -11,4 +11,48 @@ $('.track').click(function(e){
       var scrollTo = target.offset().top;
       $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
     }
+});
+
+function buttonsShare(){    
+  
+  var shareButtons = document.querySelectorAll(".button-share-media.popup-media");
+
+  if (shareButtons) {
+      [].forEach.call(shareButtons, function(button) {
+          button.addEventListener("click", function(event) {
+              var width = 650,
+                  height = 450;
+  
+              event.preventDefault();
+  
+              window.open(this.href, 'Share Dialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width='+width+',height='+height+',top='+(screen.height/2-height/2)+',left='+(screen.width/2-width/2));
+          });
+      });
+  }
+
+}
+
+function buttonGoTop(){ 
+  var btn = $('#buttonGoTop');
+  
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
   });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+}
+
+buttonGoTop();
+
+$(document).ready(function(){
+  buttonsShare();
+
+  //$('#modalForm').modal('show')
+});

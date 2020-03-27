7e33155fce241fe1adc5d7cc8a785aced18f1aa1
robson.klein <robson.klein@digitaldesk.com.br>
2020-03-27 10:12:20 -0300

    estrutura inicial

---
+++ content/contato/index.md
@@ -0,0 +1,12 @@
+---
+layout: contato
+title: Contato
+mainMenu:
+  include: true
+  order: 10000
+meta:
+  title: Contato
+  tags:
+  # - name: description
+  #   content: Conteúdo do meta content.
+---
---
+++ themes/simple-hugo/assets/javascript/script.js
@@ -0,0 +1,14 @@
+// CUSTOM JAVASCRIPT
+
+/*
+	Smooth scroll functionality for anchor links (animates the scroll
+	rather than a sudden jump in the page)
+*/
+$('.track').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+      var scrollTo = target.offset().top;
+      $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+    }
+  });
